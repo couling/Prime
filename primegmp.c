da0bfd6cb35de0be18183bf0e017e68edf875cf6
phil <philip@pedal.me.uk>
2013-08-12 09:44:35 +0000

    Completed div and mod functions
    git-svn-id: https://svn.pedal.me.uk/repos/combined/trunk@489 378dcfd6-f8a6-c149-9150-5c8ef5bc3a15

--- primegmp.c
+++ primegmp.c
@@ -4,9 +4,9 @@
 #include "primegmp.h"
 #include "primeshared.h"
 
-char * prime_to_str(char * s, Prime * prime) {
+char * prime_to_str(char * s, Prime prime) {
     Prime buffer;
-    mpn_copyd(buffer,*prime,PRIME_SIZE);
+    mpn_copyd(buffer,prime,PRIME_SIZE);
     mp_size_t i = PRIME_SIZE;
     while (i && buffer[i-1] == 0) --i;
     mp_size_t x = mpn_get_str(s,10,buffer,i);
@@ -17,7 +17,7 @@ char * prime_to_str(char * s, Prime * prime) {
 
 
 
-void str_to_prime(Prime * prime, char * s) {
+void str_to_prime(Prime prime, char * s) {
     size_t sSize = strlen(s);
     if (sSize > PRIME_STRING_SIZE) {
 	int lerrno = errno;
@@ -39,9 +39,9 @@ void str_to_prime(Prime * prime, char * s) {
         inBuffer[pos] = s[pos] - '0';
     }
 
-    mp_size_t writtenSize = mpn_set_str(*prime, inBuffer, sSize, 10);
+    mp_size_t writtenSize = mpn_set_str(prime, inBuffer, sSize, 10);
     while (writtenSize < PRIME_SIZE) {
-        (*prime)[writtenSize] = 0;
+        prime[writtenSize] = 0;
 	++writtenSize;
     }
 }
@@ -55,14 +55,42 @@ void prime_mul(Prime target, Prime in1, Prime in2) {
 }
 
 
-/*
-void prime_div_mod(Prime * div, Prime * mod, Prime * in1, Prime * in2) {
-    mpn_divmod(div, mod, 0, in1, PRIME_SIZE, in2);
+
+void prime_div_mod(Prime div, Prime mod, Prime in1, Prime in2) {
+    Prime ldiv;
+    Prime lmod;
+    p_size_t in2Size = PRIME_SIZE;
+    while (!in2[in2Size-1]&&in2Size) --in2Size;
+    mpn_tdiv_qr(ldiv, lmod, 0, in1, PRIME_SIZE, in2, in2Size);
+    mpn_copyd(div,ldiv,PRIME_SIZE);
+    mpn_copyd(mod,lmod,PRIME_SIZE);
 }
 
 
 
-void prime_sqr(Prime * target, Prime * in) {
+void prime_div(Prime div, Prime in1, Prime in2) {
+    Prime ldiv;
+    Prime lmod;
+    mp_size_t in2Size = PRIME_SIZE;
+    while (!in2[in2Size-1]&&in2Size) --in2Size;
+    mpn_tdiv_qr(ldiv, lmod, 0, in1, PRIME_SIZE, in2, in2Size);
+    mpn_copyd(div,ldiv,PRIME_SIZE);
+}
+
+
+
+void prime_mod(Prime mod, Prime in1, Prime in2) {
+    Prime ldiv;
+    Prime lmod;
+    p_size_t in2Size = PRIME_SIZE;
+    while (!in2[in2Size-1]&&in2Size) --in2Size;
+    mpn_tdiv_qr(ldiv, lmod, 0, in1, PRIME_SIZE, in2, in2Size);
+    mpn_copyd(mod,lmod,PRIME_SIZE);
+}
+
+
+
+void prime_sqr(Prime target, Prime in) {
     mp_limb_t result[PRIME_SIZE * 2];
     mpn_sqr(result, in, PRIME_SIZE);
     mpn_copyd(target, result, PRIME_SIZE);
@@ -70,6 +98,6 @@ void prime_sqr(Prime * target, Prime * in) {
 
 
 
-void prime_sqrt(Prime * target, Prime * in) {
+void prime_sqrt(Prime target, Prime in) {
 }
-*/
+
--- primegmp.h
+++ primegmp.h
@@ -3,7 +3,9 @@
 
 #include <gmp.h>
 
+#ifndef PRIME_BYTE_COUNT
 #define PRIME_BYTE_COUNT 16
+#endif // PRIME_BYTE_COUNT
 
 #define PRIME_SIZE ( PRIME_BYTE_COUNT / sizeof(mp_limb_t) )
 #define PRIME_STRING_SIZE (int)( ( ( PRIME_BYTE_COUNT * 8 * 3.01 ) / 10 ) + 1 )
@@ -11,13 +13,15 @@
 // gmp
 typedef mp_limb_t Prime[PRIME_SIZE];
 
-char * prime_to_str(char * s, Prime * prime);
-void str_to_prime(Prime * prime, char * s);
+char * prime_to_str(char * s, Prime prime);
+void str_to_prime(Prime prime, char * s);
 
 void prime_mul(Prime target, Prime in1, Prime in2);
-void prime_div(Prime * target, Prime * in1, Prime * in2);
-void prime_sqr(Prime * target, Prime * in);
-void prime_sqrt(Prime * target, Prime * in); 
+void prime_div_mod(Prime div, Prime mod, Prime in1, Prime in2);
+void prime_div(Prime div, Prime in1, Prime in2);
+void prime_mod(Prime mod, Prime in1, Prime in2);
+void prime_sqr(Prime target, Prime in);
+void prime_sqrt(Prime target, Prime in); 
 
 #define prime_gt(v1,v2) ( mpn_cmp(v1,v2,PRIME_SIZE) >  0 )
 #define prime_ge(v1,v2) ( mpn_cmp(v1,v2,PRIME_SIZE) >= 0 )
